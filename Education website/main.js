9282330e8268ecc8e58ad5b023ba4e9aa0bf6773
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-06-11 08:31:24 +0530

    change navbar color on scroll

--- Education website/main.js
+++ Education website/main.js
@@ -1,4 +1,4 @@
 // navbar on scroll : color change
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 });      //create css style for class 'window-scroll'
