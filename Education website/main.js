03e63d89d6c28f0f6358349f5f8ec7be28ffe795
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-06-11 16:39:26 +0530

    added scroll to top button , done with FAQs : toggle +/- , open/close

--- Education website/main.js
+++ Education website/main.js
@@ -1,4 +1,35 @@
-// navbar on scroll : color change
+// *** navbar on scroll : color change ***
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 });      //create css style for class 'window-scroll'
+
+
+// *** Go to top button *** 
+//Get the button
+var mybutton = document.getElementById("topBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+  
+
+// *** FAQ open toggle - show/hide answer ***
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+      faq.classList.toggle('open');   //create css style for class 'open'  //on clicking it will toggle the class 'open'
+      // # toggle the + and - icon #
+      const icon = faq.querySelector('.faq_icon i');
+      icon.classList.toggle('ri-subtract-line')
+    });                                 
+});
+
